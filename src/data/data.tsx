d33be30126fb7e5d61e0a50077f6ad3cb5de0282
Raymond Duong <iiraay@hotmail.com>
2024-05-07 21:13:51 -0600

    initial template filled and removed irrlevant sections

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,7 +1,7 @@
 import {
   AcademicCapIcon,
   ArrowDownTrayIcon,
-  BuildingOffice2Icon,
+  // BuildingOffice2Icon,
   CalendarIcon,
   FlagIcon,
   MapIcon,
@@ -11,8 +11,8 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
-import TwitterIcon from '../components/Icon/TwitterIcon';
+// import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
+// import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
@@ -72,7 +72,7 @@ export const heroData: Hero = {
   name: `I'm Raymond Duong.`,
   description: (
     <>
-      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+      <div className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
         <p>
           A Canadian-based <strong className="text-stone-100">Full Stack Software Engineer</strong> driven by a passion for innovation and impact.
           Currently seeking dynamic opportunities to contribute expertise to meaningful projects. 
@@ -81,12 +81,12 @@ export const heroData: Hero = {
           In my free time, you'll find me exploring different countries, playing or training for badminton, 
           indulging in horological timepieces, or brewing tea while seeking out the next adventure to embark upon.
         </p>
-      </p>
+      </div>
     </>
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      href: '/assets/raymond-duong-resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
@@ -116,7 +116,7 @@ export const aboutData: About = {
     {label: 'Location', text: 'Calgary, AB, Canada', Icon: MapIcon},
     {label: 'Age', text: '28', Icon: CalendarIcon},
     {label: 'Nationality', text: 'Canadian', Icon: FlagIcon},
-    {label: 'Interests', text: 'Cars, Badminton, Banjos', Icon: SparklesIcon},
+    {label: 'Interests', text: 'Cars, Traveling, Food, Watches', Icon: SparklesIcon},
     {label: 'Study', text: 'University of Calgary', Icon: AcademicCapIcon},
     // {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
   ],
@@ -126,23 +126,23 @@ export const aboutData: About = {
  * Skills section
  */
 export const skills: SkillGroup[] = [
-  {
-    name: 'Spoken languages',
-    skills: [
-      {
-        name: 'English',
-        level: 10,
-      },
-      {
-        name: 'Cantonese, Chinese',
-        level: 6,
-      },
-      {
-        name: 'Japanese',
-        level: 2,
-      },
-    ],
-  },
+  // {
+  //   name: 'Spoken languages',
+  //   skills: [
+  //     {
+  //       name: 'English',
+  //       level: 10,
+  //     },
+  //     {
+  //       name: 'Cantonese, Chinese',
+  //       level: 6,
+  //     },
+  //     {
+  //       name: 'Japanese',
+  //       level: 2,
+  //     },
+  //   ],
+  // },
   {
     name: 'Frontend development',
     skills: [
@@ -151,13 +151,13 @@ export const skills: SkillGroup[] = [
         level: 9,
       },
       {
-        name: 'Javascript',
+        name: 'JavaScript',
         level: 9,
       },
-      // {
-      //   name: 'GraphQL',
-      //   level: 6,
-      // },
+      {
+        name: 'TypeScript',
+        level: 6,
+      },
     ],
   },
   {
@@ -282,25 +282,81 @@ export const education: TimelineItem[] = [
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'Feb 2022 - Dec 2023',
+    location: 'ATB Financial - Calgary, AB, Canada (Remote)',
+    title: 'Full Stack Developer',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      <ul>
+        <li>
+          Stack: TypeScript, JavaScript, React, Node, Java, Python, Microsoft SQL Server, Git, CI/CD, ExpressJS
+        </li>
+          - Revamped legacy codebase with ReactJS, boosting customer satisfaction by 20% through improved user experience, responsiveness, and functionality.
+        <li>
+          - Led the successful migration to Simility’s fraud detection APIs, reducing fraudulent transactions by 12% and safeguarding over $100 million in assets.
+        </li>        
+        <li>
+          - Maintained a code testing coverage of 90% or higher using Jest, JUnit, and React Testing Library, ensuring software reliability and minimizing bugs in production.
+        </li>
+        <li>
+          - Designed a comprehensive development manual, unifying coding practices and fostering seamless collaboration within a 50+ engineer team.
+        </li>
+        <li>
+          - Implemented static application security testing with Veracode into gitlab CI/CD pipelines, achieving a 60% reduction in API exploits and fortifying software security.
+        </li>
+        <li>
+          - Integrated Veracode into the Gitlab CI/CD pipelines which slashed API exploits by 60%, significantly bolstering software security.
+        </li>
+      </ul>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'Oct 2019 - Feb 2022',
+    location: 'Brightside - Calgary, AB, Canada (Remote)',
+    title: 'Software Developer',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      <ul>
+        <li>
+          Stack: JavaScript, React, Node, Java, Python, MongoDB, Git, CI/CD Pipelines
+        </li>
+          - Enabled secure transactions for over 100,000 users and surpassed $100 million by integrating Interac e-Transfer, enhancing platform functionality, security, and user trust.
+        <li>
+          - Engineered a push and email notification backend microservice (Java, NodeJS, Spring Boot, MongoDB), boosting user engagement, account security, and platform robustness.
+        </li>        
+        <li>
+          - Developed the "Friends With Benefits" feature, driving significant customer acquisition by attracting new users and expanding the user base.
+        </li>
+        <li>
+          - Architected and developed a Java and React referral program, incentivizing existing users and fueling a 2,500% surge in customer acquisition and retention.
+        </li>
+        <li>
+          - Integrated Mixpanel analytics across front-end and back-end systems, enabling data-driven decisions for more effective feature prioritization.
+        </li>
+        <li>
+          - Designed and implemented various SQL query helper functions to improve code organization and visibility.
+        </li>
+      </ul>
+    ),
+  },
+  {
+    date: 'Aug 2017 - Sept 2018',
+    location: 'Arterys - Calgary, AB, Canada (Remote)',
+    title: 'Software Engineer Intern',
+    content: (
+      <ul>
+        <li>
+          Stack: JavaScript, Angular, Bash/Shell, Python, Git, Docker
+        </li>
+          - Implemented innovative automated testing designs using Pytest, accelerating the testing process and facilitating faster identification and resolution of software defects.
+        <li>
+          - Created a comprehensive on-site setup manual, saving each of the 30+ clients an estimated $10,000 by reducing reliance on costly onsite support and ensuring efficient onboarding.
+        </li>        
+        <li>
+          - Streamlined manual testing procedures, increasing release testing efficiency by 20% which resulted in reducing the man hours required per release by 10%.
+        </li>
+        <li>
+          - Developed a robust internal dashboard using Grafana empowering data-driven decision-making and bug detection and monitoring.
+        </li>
+      </ul>
     ),
   },
 ];
@@ -344,18 +400,18 @@ export const contact: ContactSection = {
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Calgary AB, Canada',
+      href: 'https://maps.app.goo.gl/qhFvs5t75f3Xd4hh9',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@iiraay',
+      href: 'https://www.instagram.com/iiraay/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'iiraay',
+      href: 'https://github.com/iiraay',
     },
   ],
 };
